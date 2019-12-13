f35e5155859d5a154e9385454f997403e1a72948
Vasiliy <Freeneos@icloud.com>
2019-12-13 19:02:02 +0300

    premake4.lua

--- Sources/premake4.lua
+++ Sources/premake4.lua
@@ -21,25 +21,3 @@ solution "libtess2"
 		includedirs { "Include", "Source" }
 		files { "Source/*.c" }
 		targetdir("Build")
-
-	-- more dynamic example
-	project "example"
-		kind "ConsoleApp"
-		language "C"
-		links { "tess2" }
-		files { "Example/example.c", "Contrib/*.c" }
-		includedirs { "Include", "Contrib" }
-		targetdir("Build")
-	 
-		configuration { "linux" }
-			 linkoptions { "`pkg-config --libs glfw3`" }
-			 links { "GL", "GLU", "m", "GLEW" }
-			 defines { "NANOVG_GLEW" }
-
-		configuration { "windows" }
-			 links { "glfw3", "gdi32", "winmm", "user32", "GLEW", "glu32","opengl32" }
-			 defines { "NANOVG_GLEW" }
-
-		configuration { "macosx" }
-			links { "glfw3" }
-			linkoptions { "-framework OpenGL", "-framework Cocoa", "-framework IOKit", "-framework CoreVideo" }
